993e50d0ac8d764ec7bde377f4b5198c8171119b
LoveClean <641655770@qq.com>
2019-07-15 22:19:18 +0800

    注释更新

--- src/main/java/com/springboot/framework/ProjectApplication.java
+++ src/main/java/com/springboot/framework/ProjectApplication.java
@@ -6,12 +6,14 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import tk.mybatis.spring.annotation.MapperScan;
 
+/**
+ * @author huangpengfei
+ */
 @MapperScan(value = "com.springboot.framework.dao.mapper")
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
 public class ProjectApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
     }
