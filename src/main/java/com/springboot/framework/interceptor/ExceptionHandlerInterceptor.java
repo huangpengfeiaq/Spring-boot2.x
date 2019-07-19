05162fbec218407254e560fa4eda5abfc4e913d2
LoveClean <641655770@qq.com>
2019-07-19 21:31:29 +0800

    更新了注释

--- src/main/java/com/springboot/framework/interceptor/ExceptionHandlerInterceptor.java
+++ src/main/java/com/springboot/framework/interceptor/ExceptionHandlerInterceptor.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
+ * 全局异常拦截器
+ *
  * @author huangpengfei
  * @version 1.0
  * @date 2019/7/8 22:37
