efdec0e88648da9aadaaa556b2ea5bf21ce8efba
LoveClean <641655770@qq.com>
2019-07-27 13:01:35 +0800

    归并排序

--- src/main/java/com/springboot/framework/datastructures/sort/MergeSort.java
+++ src/main/java/com/springboot/framework/datastructures/sort/MergeSort.java
@@ -1,5 +1,7 @@
 package com.springboot.framework.datastructures.sort;
 
+import java.util.Arrays;
+
 /**
  * 归并排序
  *
@@ -10,7 +12,25 @@ package com.springboot.framework.datastructures.sort;
 public class MergeSort {
     public static void main(String[] args) {
         int[] arr = {8, 4, 5, 7, 1, 3, 6, 2};
-        mergeSort(arr);
+        int[] temp = new int[arr.length];
+        mergeSor(arr, 0, 7, temp);
+    }
+
+    /**
+     * 归并排序
+     */
+    static void mergeSor(int[] arr, int left, int right, int[] temp) {
+        if (left < right) {
+            // 中间索引
+            int mid = (left + right) / 2;
+            // 向左递归进行分解
+            mergeSor(arr, left, mid, temp);
+            // 向右递归进行分解
+            mergeSor(arr, mid + 1, right, temp);
+
+            // 合并
+            merge(arr, left, mid, right, temp);
+        }
     }
 
     /**
@@ -22,12 +42,52 @@ public class MergeSort {
      * @param right 右边索引
      * @param temp  做中转的数组
      */
-    static void merge(int[] arr, int left, int mid, int right, int[] temp) {
-    }
+    private static void merge(int[] arr, int left, int mid, int right, int[] temp) {
+        // 初始化i，左边有序序列的初始索引
+        int i = left;
+        // 初始化j，右边有序序列的初始索引
+        int j = mid + 1;
+        // 初始化t，temp数组的当前索引
+        int t = 0;
 
-    /**
-     * 归并排序
-     */
-    static void mergeSort(int[] arr) {
+        // 先把左右两边（有序）的数据按照规则填充到temp数组
+        // 直到左右两边的有序序列，有一边处理完毕为止
+//        while (i <= mid && j <= right) {
+//            if (arr[i] <= arr[j]) {
+//                temp[t++] = arr[i++];
+//            } else if (arr[i] > arr[j]) {
+//                temp[t++] = arr[j++];
+//            }
+//        }
+        for (; i <= mid && j <= right; ) {
+            if (arr[i] <= arr[j]) {
+                temp[t++] = arr[i++];
+            } else if (arr[i] > arr[j]) {
+                temp[t++] = arr[j++];
+            }
+        }
+
+        // 把有剩余数据的一边的数据依次全部填充到temp
+        if (i == mid + 1) {
+            for (; j <= right; ) {
+                temp[t++] = arr[j++];
+            }
+        } else if (j == right + 1) {
+            for (; i <= mid; ) {
+                temp[t++] = arr[i++];
+            }
+        }
+
+        //Todo 不太理解！！
+        // 将temp数组的元素全部拷贝到arr
+        // 注意，不是每次都拷贝所有
+        t = 0;
+        int tempLeft = left;
+        while (tempLeft <= right) {
+            arr[tempLeft++] = temp[t++];
+        }
+
+        System.out.println(Arrays.toString(temp));
+        System.out.println(Arrays.toString(arr));
     }
 }
