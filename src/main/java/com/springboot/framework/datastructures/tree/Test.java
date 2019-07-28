421ffaa748e3c28c5d89904ff6317e6099ad19f9
LoveClean <641655770@qq.com>
2019-07-28 18:28:12 +0800

    二叉树

---
+++ src/main/java/com/springboot/framework/datastructures/tree/Test.java
@@ -0,0 +1,15 @@
+package com.springboot.framework.datastructures.tree;
+
+import java.util.ArrayList;
+
+/**
+ * @author huangpengfei
+ * @version 1.0
+ * @date 2019/7/28 15:12
+ */
+public class Test {
+    public static void main(String[] args) {
+        // 以ArrayList 为例，看看是怎样进行数组扩容
+        ArrayList arrayList = new ArrayList();
+    }
+}
