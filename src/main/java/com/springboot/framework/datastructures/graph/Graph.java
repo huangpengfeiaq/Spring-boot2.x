57e1c8313f8308490c122dcd7c49999d879ce949
LoveClean <641655770@qq.com>
2019-07-31 00:46:19 +0800

    图

---
+++ src/main/java/com/springboot/framework/datastructures/graph/Graph.java
@@ -0,0 +1,120 @@
+package com.springboot.framework.datastructures.graph;
+
+import java.util.ArrayList;
+
+/**
+ * 图
+ *
+ * @author huangpengfei
+ * @version 1.0
+ * @date 2019/7/30 23:56
+ */
+public class Graph {
+    public static void main(String[] args) {
+        // 测试
+        // 顶点的个数
+        int n = 5;
+        // 顶点的值
+        String[] vertexValues = {"A", "B", "C", "D", "E"};
+        // 创建图对象
+        Graph graph = new Graph(n);
+        // 插入顶点
+        for (String v : vertexValues) {
+            graph.insertVertex(v);
+        }
+        // 添加边
+        // A-B、A-C、B-C、B-D、B-E
+        graph.insertEdges(0, 1, 1);
+        graph.insertEdges(0, 2, 1);
+        graph.insertEdges(1, 2, 1);
+        graph.insertEdges(1, 3, 1);
+        graph.insertEdges(1, 4, 1);
+        // 显示
+        graph.showGraph();
+    }
+
+    /**
+     * 存储顶点集合
+     */
+    private ArrayList<String> vertexList;
+    /**
+     * 存储图对应的邻接矩阵
+     */
+    private int[][] edges;
+    /**
+     * 表示边的数目
+     */
+    private int numOfEdges;
+
+    /**
+     * 构造器
+     *
+     * @param n 顶点个数
+     */
+    public Graph(int n) {
+        // 初始化矩阵和vertexList
+        edges = new int[n][n];
+        vertexList = new ArrayList<>(n);
+        numOfEdges = 0;
+    }
+
+    /**
+     * 显示图对应的矩阵
+     */
+    public void showGraph() {
+        for (int[] i : edges) {
+            for (int j : i) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * 返回v1 和v2 的权值
+     */
+    public int getWeight(int v1, int v2) {
+        return edges[v1][v2];
+    }
+
+    /**
+     * 返回顶点i对应的数据 0->"A" 1->"B"
+     */
+    public String getValueByIndex(int i) {
+        return vertexList.get(i);
+    }
+
+    /**
+     * 返回顶点的个数
+     */
+    public int getNumOfVertex() {
+        return vertexList.size();
+    }
+
+    /**
+     * 返回边的个数
+     */
+    public int getNumOfEdges() {
+        return numOfEdges;
+    }
+
+    /**
+     * 插入顶点
+     */
+    public void insertVertex(String vertex) {
+        vertexList.add(vertex);
+    }
+
+    /**
+     * 添加边
+     *
+     * @param v1     表示第一个顶点的对应下标，即第几个顶点("A"-"B" "A"->0 "B"->1)
+     * @param v2     表示第二个顶点的对应下标
+     * @param weight 权值
+     */
+    public void insertEdges(int v1, int v2, int weight) {
+        edges[v1][v2] = weight;
+        edges[v2][v1] = weight;
+        numOfEdges++;
+    }
+}
