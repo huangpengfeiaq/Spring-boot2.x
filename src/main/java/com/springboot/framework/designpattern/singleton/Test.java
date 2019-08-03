8940e817363e2e6441120d49cb7b215e1c17ec93
LoveClean <641655770@qq.com>
2019-08-03 16:09:59 +0800

    单例模式（jdk源码）

---
+++ src/main/java/com/springboot/framework/designpattern/singleton/Test.java
@@ -0,0 +1,14 @@
+package com.springboot.framework.designpattern.singleton;
+
+/**
+ * @author huangpengfei
+ * @version 1.0
+ * @date 2019/8/3 12:21
+ */
+public class Test {
+    public static void main(String[] args) {
+        // JDK 中源码使用单例模式场景
+        // Runtime 使用了饿汉式
+        Runtime runtime = Runtime.getRuntime();
+    }
+}
