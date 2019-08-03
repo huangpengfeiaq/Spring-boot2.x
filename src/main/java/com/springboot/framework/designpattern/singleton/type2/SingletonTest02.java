4369f1f3377c92bb960726074f404f228d20d5c7
LoveClean <641655770@qq.com>
2019-08-03 11:34:20 +0800

    单例模式（饿汉式-静态代码块）

---
+++ src/main/java/com/springboot/framework/designpattern/singleton/type2/SingletonTest02.java
@@ -0,0 +1,47 @@
+package com.springboot.framework.designpattern.singleton.type2;
+
+/**
+ * 单例模式（饿汉式-静态代码块）
+ *
+ * @author huangpengfei
+ * @version 1.0
+ * @date 2019/8/3 11:08
+ */
+public class SingletonTest02 {
+    public static void main(String[] args) {
+        // 测试
+        Singleton instance = Singleton.getInstance();
+        Singleton instance2 = Singleton.getInstance();
+        System.out.println(instance == instance2);
+        System.out.println("instance.hashCode=" + instance.hashCode());
+        System.out.println("instance2.hashCode=" + instance2.hashCode());
+    }
+}
+
+/**
+ * 饿汉式（静态代码块）
+ */
+class Singleton {
+    /**
+     * 1.构造器私有化，外部不能new
+     */
+    private Singleton() {
+    }
+
+    /**
+     * 2.本类内部创建对象实例
+     */
+    private static Singleton instance;
+
+    // 在静态代码块中，创建单例对象
+    static {
+        instance = new Singleton();
+    }
+
+    /**
+     * 3.提供一个共有的静态方法，返回实例对象
+     */
+    public static Singleton getInstance() {
+        return instance;
+    }
+}
