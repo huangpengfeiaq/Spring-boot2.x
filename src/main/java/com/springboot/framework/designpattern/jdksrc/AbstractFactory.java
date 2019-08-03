c45ddeb56a0f5a49add0851b619413de71b984d7
LoveClean <641655770@qq.com>
2019-08-04 00:46:04 +0800

    JDK 中源码使用工厂模式场景

---
+++ src/main/java/com/springboot/framework/designpattern/jdksrc/AbstractFactory.java
@@ -0,0 +1,15 @@
+package com.springboot.framework.designpattern.jdksrc;
+
+import java.util.Calendar;
+
+/**
+ * @author huangpengfei
+ * @version 1.0
+ * @date 2019/8/4 0:38
+ */
+public class AbstractFactory {
+    public static void main(String[] args) {
+        // getInstance 是 Calendar 静态方法
+        Calendar calendar = Calendar.getInstance();
+    }
+}
