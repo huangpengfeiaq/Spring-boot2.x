18e5969389579e1568d5ca2f24e2178bb9babc35
LoveClean <641655770@qq.com>
2019-08-04 00:37:56 +0800

    JDK 中源码使用单例模式场景

--- src/main/java/com/springboot/framework/designpattern/jdksrc/Singleton.java
+++ src/main/java/com/springboot/framework/designpattern/jdksrc/Singleton.java
@@ -1,11 +1,11 @@
-package com.springboot.framework.designpattern.singleton;
+package com.springboot.framework.designpattern.jdksrc;
 
 /**
  * @author huangpengfei
  * @version 1.0
  * @date 2019/8/3 12:21
  */
-public class Test {
+public class Singleton {
     public static void main(String[] args) {
         // JDK 中源码使用单例模式场景
         // Runtime 使用了饿汉式
