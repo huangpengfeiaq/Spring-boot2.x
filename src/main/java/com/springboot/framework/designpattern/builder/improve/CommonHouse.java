32ac5b2d6e16061a51ed0e60f18e878524ad4677
LoveClean <641655770@qq.com>
2019-08-04 16:13:08 +0800

    建造者模式

---
+++ src/main/java/com/springboot/framework/designpattern/builder/improve/CommonHouse.java
@@ -0,0 +1,23 @@
+package com.springboot.framework.designpattern.builder.improve;
+
+/**
+ * @author huangpengfei
+ * @version 1.0
+ * @date 2019/8/4 16:03
+ */
+public class CommonHouse extends HouseBuilder {
+    @Override
+    public void buildBasic() {
+        System.out.println(" 普通房子打地基5米");
+    }
+
+    @Override
+    public void buildWalls() {
+        System.out.println(" 普通房子打砌墙10米");
+    }
+
+    @Override
+    public void roofed() {
+        System.out.println(" 普通房子盖普通屋顶");
+    }
+}
