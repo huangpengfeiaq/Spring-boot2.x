6bd1697ce96404f85e55f5e72f8c233da860ddf1
LoveClean <641655770@qq.com>
2019-08-04 23:48:09 +0800

    建造者模式

---
+++ src/main/java/com/springboot/framework/designpattern/sourcecode/jdksrc/Builder.java
@@ -0,0 +1,13 @@
+package com.springboot.framework.designpattern.sourcecode.jdksrc;
+
+/**
+ * @author huangpengfei
+ * @version 1.0
+ * @date 2019/8/4 16:17
+ */
+public class Builder {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder("hello,world");
+        System.out.println(stringBuilder);
+    }
+}
