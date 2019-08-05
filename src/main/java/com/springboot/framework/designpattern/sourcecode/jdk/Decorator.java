f0a88d904a5a851bbac6e68b864ce58ad2305ab3
LoveClean <641655770@qq.com>
2019-08-05 12:40:31 +0800

    装饰者模式

---
+++ src/main/java/com/springboot/framework/designpattern/sourcecode/jdk/Decorator.java
@@ -0,0 +1,15 @@
+package com.springboot.framework.designpattern.sourcecode.jdk;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+
+/**
+ * @author huangpengfei
+ * @version 1.0
+ * @date 2019/8/5 12:38
+ */
+public class Decorator {
+    public static void main(String[] args) throws Exception {
+        new DataInputStream(new FileInputStream(""));
+    }
+}
