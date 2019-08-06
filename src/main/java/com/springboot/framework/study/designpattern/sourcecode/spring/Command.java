ed57ca540548a9422cff84b98df4413c3c293161
LoveClean <641655770@qq.com>
2019-08-06 11:35:30 +0800

    命令模式

---
+++ src/main/java/com/springboot/framework/study/designpattern/sourcecode/spring/Command.java
@@ -0,0 +1,16 @@
+package com.springboot.framework.study.designpattern.sourcecode.spring;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * 命令模式
+ *
+ * @author huangpengfei
+ * @version 1.0
+ * @date 2019/8/6 11:34
+ */
+public class Command {
+    public static void main(String[] args) {
+        JdbcTemplate jdbcTemplate = null;
+    }
+}
