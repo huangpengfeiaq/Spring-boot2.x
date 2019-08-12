6434b0840ca890a2804a9a8e8365429dc1d85313
LoveClean <641655770@qq.com>
2019-08-12 23:39:15 +0800

    演示线程的死锁问题

---
+++ src/main/java/com/springboot/framework/study/javasenior/thread/ThreadLockTest.java
@@ -0,0 +1,69 @@
+package com.springboot.framework.study.javasenior.thread;
+
+/**
+ * 演示线程的死锁问题
+ * <p>
+ * 死锁的理解：不同的线程分别占用对方需要的同步资源不放弃，都在等待对方放弃自己需要的同步资源，就形成了线程的死锁
+ *
+ * @author huangpengfei
+ * @version 1.0
+ * @date 2019/8/12 23:26
+ */
+public class ThreadLockTest {
+    public static void main(String[] args) {
+        StringBuffer s1 = new StringBuffer();
+        StringBuffer s2 = new StringBuffer();
+
+        new Thread() {
+            @Override
+            public void run() {
+
+                synchronized (s1) {
+                    s1.append("a");
+                    s2.append("1");
+
+                    try {
+                        sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    synchronized (s2) {
+                        s1.append("b");
+                        s2.append("2");
+
+                        System.out.println(s1);
+                        System.out.println(s2);
+                    }
+                }
+
+            }
+        }.start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                synchronized (s2) {
+                    s1.append("c");
+                    s2.append("3");
+
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    synchronized (s1) {
+                        s1.append("d");
+                        s2.append("4");
+
+                        System.out.println(s1);
+                        System.out.println(s2);
+                    }
+                }
+
+            }
+        }).start();
+    }
+}
