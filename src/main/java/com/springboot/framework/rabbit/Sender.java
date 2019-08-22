280bc651032b88e7048e64e84541d349d919f8dc
LoveClean <641655770@qq.com>
2019-08-22 15:46:58 +0800

    移除了rabbitMQ

--- src/main/java/com/springboot/framework/rabbit/Sender.java
+++ src/main/java/com/springboot/framework/rabbit/Sender.java
@@ -1,59 +1,59 @@
-package com.springboot.framework.rabbit;
-
-import javax.annotation.PostConstruct;
-
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.rabbit.connection.CorrelationData;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.core.RabbitTemplate.ReturnCallback;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import java.util.UUID;
-
-/**
- * @author huangpengfei
- * @version 1.0
- * @date 2019/7/21 11:33
- */
-@Component
-public class Sender implements RabbitTemplate.ConfirmCallback, ReturnCallback {
-
-    @Autowired
-    private RabbitTemplate rabbitTemplate;
-
-    @PostConstruct
-    public void init() {
-        rabbitTemplate.setConfirmCallback(this);
-        rabbitTemplate.setReturnCallback(this);
-    }
-
-    @Override
-    public void confirm(CorrelationData correlationData, boolean ack, String cause) {
-        if (ack) {
-            System.out.println("消息发送成功:" + correlationData);
-        } else {
-            System.out.println("消息发送失败:" + cause);
-        }
-
-    }
-
-    @Override
-    public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
-        System.out.println(message.getMessageProperties().getCorrelationId() + " 发送失败");
-
-    }
-
-    /**
-     * 发送消息，不需要实现任何接口，供外部调用。
-     */
-    public void send(String msg) {
-
-        CorrelationData correlationId = new CorrelationData(UUID.randomUUID().toString());
-
-        System.out.println("开始发送消息 : " + msg.toLowerCase());
-        String response = rabbitTemplate.convertSendAndReceive("topicExchange", "key.1", msg, correlationId).toString();
-        System.out.println("结束发送消息 : " + msg.toLowerCase());
-        System.out.println("消费者响应 : " + response + " 消息处理完成");
-    }
-}
+//package com.springboot.framework.rabbit;
+//
+//import javax.annotation.PostConstruct;
+//
+//import org.springframework.amqp.core.Message;
+//import org.springframework.amqp.rabbit.connection.CorrelationData;
+//import org.springframework.amqp.rabbit.core.RabbitTemplate;
+//import org.springframework.amqp.rabbit.core.RabbitTemplate.ReturnCallback;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.UUID;
+//
+///**
+// * @author huangpengfei
+// * @version 1.0
+// * @date 2019/7/21 11:33
+// */
+//@Component
+//public class Sender implements RabbitTemplate.ConfirmCallback, ReturnCallback {
+//
+//    @Autowired
+//    private RabbitTemplate rabbitTemplate;
+//
+//    @PostConstruct
+//    public void init() {
+//        rabbitTemplate.setConfirmCallback(this);
+//        rabbitTemplate.setReturnCallback(this);
+//    }
+//
+//    @Override
+//    public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+//        if (ack) {
+//            System.out.println("消息发送成功:" + correlationData);
+//        } else {
+//            System.out.println("消息发送失败:" + cause);
+//        }
+//
+//    }
+//
+//    @Override
+//    public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
+//        System.out.println(message.getMessageProperties().getCorrelationId() + " 发送失败");
+//
+//    }
+//
+//    /**
+//     * 发送消息，不需要实现任何接口，供外部调用。
+//     */
+//    public void send(String msg) {
+//
+//        CorrelationData correlationId = new CorrelationData(UUID.randomUUID().toString());
+//
+//        System.out.println("开始发送消息 : " + msg.toLowerCase());
+//        String response = rabbitTemplate.convertSendAndReceive("topicExchange", "key.1", msg, correlationId).toString();
+//        System.out.println("结束发送消息 : " + msg.toLowerCase());
+//        System.out.println("消费者响应 : " + response + " 消息处理完成");
+//    }
+//}
